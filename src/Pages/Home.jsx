9d6f195a2ce450a777b05cb2edd0b8dee7be29e1
chandan-kumar-au7 <chandankr.pra930@gmail.com>
2020-12-11 09:48:08 +0530

    sending my code for valuation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "../Styles/home.css";
+import "../Styles/PurchasedService.css";
+
 import axios from "axios";
 
 import PricePage from "./PricePage";
 import AdditionalServices from "./AdditionalServices";
-import PurchasedService from "./PurchasedService";
+// import PurchasedService from "./PurchasedService";
 
 import Card from "../Components/Card";
 
@@ -12,7 +14,8 @@ const HomePage = () => {
   const [pData, setpData] = useState([]);
   const [aData, setaData] = useState([]);
 
-  const [Updated, setUpdated] = useState(false);
+  let newPDATA = [];
+  let newADATA = [];
 
   const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
   const URL = "https://fir-dynamiclinks-e43dd.web.app/practical-api.json";
@@ -41,7 +44,7 @@ const HomePage = () => {
               //   data[i].purchased_office_template.purchased_office_services[j]
               // );
 
-              setpData(
+              newPDATA.push(
                 data[i].purchased_office_template.purchased_office_services[j]
               );
               // setpData(
@@ -60,11 +63,9 @@ const HomePage = () => {
               //   data[i].purchased_office_template.purchased_office_services[j]
               // );
 
-              setaData(
+              newADATA.push(
                 data[i].purchased_office_template.purchased_office_services[j]
               );
-
-              setUpdated(true);
 
               // setaData(
               //   data[i].purchased_office_template.purchased_office_services[j]
@@ -77,38 +78,50 @@ const HomePage = () => {
         //   "length 1 ",
         //   data[i].purchased_office_template.purchased_office_services.length
         // );
+
+        setpData(newPDATA);
+        setaData(newADATA);
       })
       .catch((err) => console.error("Error occured! ", err));
 
     // console.log("Purchased Services : ===== >  \n", pData, "\n");
     // console.log("Additional Services : ===== > \n", aData, "\n");
-  }, [Updated]);
+  }, []);
 
   return (
     <div className="homeDiv">
-      {/* {pData.map((data11) => {
-        console.log(data11.length);
-      })} */}
-      <PurchasedService
-        card={
-          <Card
-            mainserviceno={"1"}
-            src={
-              "https://th.bing.com/th/id/OIP.bircyBDvJOcKd3mkR6ramwHaEK?w=313&h=180&c=7&o=5&pid=1.7"
-            }
-            srcAlt={"img"}
-            servicesno={"1"}
-            description={"description"}
-            price={"2000"}
-          />
-        }
-      />
+      {/* {{{{{{{{{{}}}}}}}}}} Purchased Services from here {{{{{{{{{{}}}}}}}}}} */}
+      <div className="parentOfPurchpage">
+        <h2 className="PURCHASEDh2">PURCHASED SERVICES</h2>
+        <p>Showing Purchased Services List</p>
+        <div>
+          {pData.map((pData11, key) => {
+            console.log("pData11 ", pData11);
+
+            return (
+              <Card
+                key={key}
+                mainserviceno={pData11.name}
+                src={pData11.image}
+                srcAlt={pData11.name}
+                servicesno={pData11.id}
+                description={pData11.description}
+                price={pData11.price}
+              />
+            );
+          })}
+        </div>
+      </div>
+      {/* {{{{{{{{{{}}}}}}}}}} End Of Purchased Services from here {{{{{{{{{{}}}}}}}}}} */}
 
       <PricePage />
       {/* {AdditionalServicesData.map((data22) => {
         console.log("AdditionalServicesData ", data22);
         return null;
       })} */}
+
+      {/* {{{{{{{{{{}}}}}}}}}} Additional Services from here {{{{{{{{{{}}}}}}}}}} */}
+
       <AdditionalServices
         card={
           <Card
@@ -123,6 +136,7 @@ const HomePage = () => {
           />
         }
       />
+      {/* {{{{{{{{{{}}}}}}}}}} Additional Services from here {{{{{{{{{{}}}}}}}}}} */}
     </div>
   );
 };
